b14ec6e8b969b8902ad591dc8f76fef2ecd4a8f1
Student <Student@LabVM2049939.yrwr0n2qv2tuddvdprpss3nsnh.jnbx.internal.cloudapp.net>
2026-09-15 17:49:46 +0200

    sorted file structure and added variables for the login file

---
+++ src/main/java/za/ac/iie/prog5121/Login.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.iie.prog5121;
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+    
+     // Variables used to store the registered user's details
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+    
+}
